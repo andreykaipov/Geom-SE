9f3732383688b5ffc06e3c54f13988cf1538fafc
Andrey Kaipov <akaip002@fiu.edu>
2016-02-10 01:31:11 -0500

    Improve triangulate script by removing superfluous data.

--- triangulate.rb
+++ triangulate.rb
@@ -1,6 +1,10 @@
 # ARGV[0] is the source .obj file to be triangulated.
 # ARGV[1] is the triangulated .obj file with a name of your choice.
 #
+# Further, this script removes vertex texture coordinates, vertex normals,
+# and parameter space vertices. See https://en.wikipedia.org/wiki/Wavefront_.obj_file#File_format.
+# We compute our own face normals and vertex normals.
+#
 # Example:
 # ruby triangulate.rb ../3D_Models/82porsche.obj 82porsche-triangulated.obj
 
@@ -10,7 +14,10 @@ File.open( ARGV[0] ) do |file|
 
     file.each_line do |line|
 
-        if ( line[0] == 'f' )
+        first_char = line[0];
+        second_char = line[1];
+
+        if ( first_char == 'f' && second_char == ' ' )
 
             vertices = line.slice(2..-1).split(' ')
             fixed_vertex = vertices[0];
@@ -21,7 +28,7 @@ File.open( ARGV[0] ) do |file|
                 output << "\n"
             end
 
-        else
+        elsif ( first_char == 'v' && second_char == ' ' )
 
             output << line
 
