395d5238ed9b431e67e38f040427fedd2684931c
Andrey Kaipov <akaip002@fiu.edu>
2016-02-13 02:23:18 -0500

    Add in WASD controls to obj viewer. But they're not that great...

--- obj_viewer/js/obj_viewer.js
+++ obj_viewer/js/obj_viewer.js
@@ -4,16 +4,21 @@
 //
 // This program demonstrates how we can use Three.js for 3D Graphics tasks.
 // Three.js is a fairly lightweight 3D library using WebGL. http://threejs.org/
-//
-// to do:
-// - triangulation of .obj files !!!
-// - make wasd camera controllable. scroll-wheel only zooms.
-// - allow objects to be selectable
-// - add customizable lights.
-// - allow translation of added objects, because objects all clump together at
-// the origin.
-// - allow fps be customizable..
-// - add standard geometries (cubes, spheres, etc.)
+
+/*
+to do:
+- triangulation of .obj files !!! DONE !!!
+- make wasd camera controllable. scroll-wheel only zooms.
+    Kinda done. It could be better. The OrbitControls mess with the keyboard input,
+    so I just made the keyboard controls just rotate around the origin or something like that.
+    I might just take these out...
+- allow objects to be selectable
+- add customizable lights.
+- allow translation of added objects, because objects all clump together at
+the origin.
+- allow fps be customizable..
+- add standard geometries (cubes, spheres, etc.)
+*/
 
 init();
 lights();
@@ -24,8 +29,9 @@ animate();
 // The camera is what looks at the scene.
 // The renderer will display our beautifully crafted scene.
 // The controls allow you to move around the scene with the camera.
-var scene, camera, renderer, cameraControls;
+var scene, camera, renderer, mouseControls;
 var loadedObject, loadedObjectMesh;
+var keyboard;
 
 /* Initializes our scene, camera, renderer, and controls. */
 function init() {
@@ -53,7 +59,8 @@ function init() {
     renderer.setSize( canvasWidth, canvasHeight );
 
     // Add controls so we can pan around with the mouse and arrow-keys.
-    cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+    keyboard = new THREEx.KeyboardState();
+    mouseControls = new THREE.OrbitControls( camera, renderer.domElement );
 
     // Adds debug axes centered at (0,0,0). Remember: xyz ~ rgb. Solid is positive, dashed is negative.
     createAxes( 100 );
@@ -107,7 +114,7 @@ function lights() {
 // within a dummy Object3D, but it's from mrdoob himself http://stackoverflow.com/a/22977590/4085283
 // We want for this container to be global so that scaling and rotational properties can be inherited by
 // any of the objects put inside this container. See gui.js for that.
-var objContainer = new THREE.Object3D;
+objContainer = new THREE.Object3D;
 
 /* Loads our 3D Object and returns it. */
 function create3DObject( obj_url, obj_name, obj_material ) {
@@ -180,15 +187,13 @@ function animate() {
     }, 1000 / 30 ); // frame count goes to the denominator.
 
     render();
+    keyboardUpdate();
 }
 
 
 /* This just renders once. */
 function render() {
-
     renderer.render( scene, camera );
-    cameraControls.update();
-
 }
 
 
@@ -200,3 +205,41 @@ window.addEventListener( "resize", function() {
     camera.aspect = canvasWidth / canvasHeight;
     camera.updateProjectionMatrix();
 });
+
+function keyboardUpdate() {
+
+    // Get the vector representing the direction in which the camera is looking.
+    var dirVector = camera.getWorldDirection().multiplyScalar(0.10);
+
+	if ( keyboard.pressed("W") ) {
+        camera.position.add( dirVector );
+        camera.lookAt( mouseControls.target = camera.getWorldDirection() );
+    }
+	else if ( keyboard.pressed("S") ) {
+        camera.position.sub( dirVector );
+        camera.lookAt( mouseControls.target = camera.getWorldDirection() );
+    }
+	else if ( keyboard.pressed("A") ) {
+        camera.position.sub( dirVector.cross( new THREE.Vector3( 0, 1, 0) ) );
+        camera.lookAt( mouseControls.target = camera.getWorldDirection() );
+    }
+	else if ( keyboard.pressed("D") ) {
+        camera.position.add( dirVector.cross( new THREE.Vector3( 0, 1, 0) ) );
+        camera.lookAt( mouseControls.target = camera.getWorldDirection() );
+    }
+    else if ( keyboard.pressed("F") ) {
+        camera.position.sub( new THREE.Vector3(0, 0.1, 0) );
+        camera.lookAt( mouseControls.target = camera.getWorldDirection() );
+    }
+	else if ( keyboard.pressed("R") ) {
+        camera.position.add( new THREE.Vector3(0, 0.1, 0) );
+        camera.lookAt( mouseControls.target = camera.getWorldDirection() );
+    }
+	else if ( keyboard.pressed("Z") )
+	{
+        camera.position.set( 0, 0, 5 );
+        mouseControls.target.set( 0, 0, 0 );
+        camera.lookAt( 0, 0, 0 );
+	}
+    camera.updateProjectionMatrix();
+}
