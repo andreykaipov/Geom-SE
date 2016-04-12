500f55ae6d5bba983ac7843b30b4a570c46e1379
Andrey Kaipov <akaip002@fiu.edu>
2016-04-12 01:12:33 -0400

    Add obj exports

--- js/GFXGUI.js
+++ js/GFXGUI.js
@@ -21,11 +21,50 @@ class GFXGUI {
 
     createGUI() {
 
-        this.__createGUIScale( this.gui );
+        this.__create_GUI_scale( this.gui );
+        this.__create_GUI_rotation( this.gui );
+        this.__create_GUI_translation( this.gui );
+        this.__create_upload_obj_file( this.gui );
+        this.__create_export_obj_file( this.gui );
 
     }
 
-    __createGUIScale( gui ) {
+    __create_upload_obj_file( gui ) {
+
+        gui.add({
+            uploadObj: function() {
+
+                document.getElementById("i_file").click();
+
+            }
+        }, 'uploadObj').name( "upload an obj file" );
+
+    }
+
+    __create_export_obj_file( gui ) {
+
+        let self = this;
+
+        gui.add({
+            exportObj: function() {
+
+                let exporter = new THREE.OBJExporter();
+
+                let objAsString = exporter.parse( self.gfxViewer.selectedObject );
+                let objAsFile = new Blob( [ objAsString ], { type: "text/plain" } );
+                let objFilePath = window.URL.createObjectURL( objAsFile );
+
+                let link = document.getElementById("o_file")
+                link.href = objFilePath;
+                link.download = self.gfxViewer.selectedObject.name.slice(0,-4) + "_modified.obj";
+                link.click()
+
+            }
+        }, 'exportObj').name( "export as obj" );
+
+    }
+
+    __create_GUI_scale( gui ) {
 
         var parameters = {
             scaleX : 1,
@@ -69,6 +108,104 @@ class GFXGUI {
                 scaleFolder.__controllers[i].updateDisplay();
         }
 
+    }
+
+    __create_GUI_rotation( gui ) {
+        // We need to start off with non-integer values because dat.gui is a little buggy.
+        // See https://github.com/dataarts/dat.gui/issues/48 for more info.
+        var parameters = {
+            rotationX : 0.001,
+            rotationY : 0.001,
+            rotationZ : 0.001
+        };
+
+        var rotationFolder = gui.addFolder( "Rotation (clockwise in radians)" );
+
+        rotationFolder.add( parameters, 'rotationX', -Math.PI, Math.PI ).name( "rotate on x" ).onChange(
+            function ( value ) { gfxViewer.selectedMesh.rotation.x = value; }
+        );
+        rotationFolder.add( parameters, 'rotationY', -Math.PI, Math.PI ).name( "rotate on y" ).onChange(
+            function( value ) {
+                gfxViewer.selectedMesh.rotation.y = value;
+            }
+        );
+        rotationFolder.add( parameters, 'rotationZ', -Math.PI, Math.PI ).name( "rotate on z" ).onChange(
+            function( value ) { gfxViewer.selectedMesh.rotation.z = value; }
+        );
+
+        // Set the sliders to start at 0. This is related to the above bug issue.
+        parameters.rotationX = parameters.rotationY = parameters.rotationZ = 0;
+
+        rotationFolder.add( {
+            // Reset object rotation and reset gui sliders.
+            resetRotation: function() {
+                parameters.rotationX = parameters.rotationY = parameters.rotationZ = 0;
+                gfxViewer.selectedMesh.rotation.set( 0, 0, 0 );
+            }
+        }, 'resetRotation' ).name( "reset rotation" );
+
+        // When an object is moved with the TransformControls (see obj_viewer.js),
+        // the GUI sliders need to be visually updated, so we do that here.
+        document.addEventListener( 'mousedown', updateRotationGui );
+
+        function updateRotationGui() {
+            requestAnimationFrame( updateRotationGui );
+            if ( gfxViewer.selectedMesh != null ) {
+                parameters.rotationX = gfxViewer.selectedMesh.rotation.x;
+                parameters.rotationY = gfxViewer.selectedMesh.rotation.y;
+                parameters.rotationZ = gfxViewer.selectedMesh.rotation.z;
+            }
+            for ( var i in rotationFolder.__controllers )
+            rotationFolder.__controllers[i].updateDisplay();
+        }
+
+    }
+
+    __create_GUI_translation( gui ) {
+        var parameters = {
+            translationX : 0.01,
+            translationY : 0.01,
+            translationZ : 0.01
+        };
+
+        var translationFolder = gui.addFolder( "Translation" );
+
+        translationFolder.add( parameters, 'translationX', -20, 20 ).name( "translate x" ).onChange(
+            function ( value ) { gfxViewer.selectedMesh.position.setX( value ); }
+        );
+        translationFolder.add( parameters, 'translationY', -20, 20 ).name( "translate y" ).onChange(
+            function( value ) { gfxViewer.selectedMesh.position.setY( value ); }
+        );
+        translationFolder.add( parameters, 'translationZ', -20, 20 ).name( "translate z" ).onChange(
+            function( value ) { gfxViewer.selectedMesh.position.setZ( value ); }
+        );
+
+        // Set the sliders to start at 0.
+        parameters.translationX = parameters.translationY = parameters.translationZ = 0;
+
+        translationFolder.add( {
+            // Reset object translation and reset gui sliders.
+            resetTranslation: function() {
+                parameters.translationX = parameters.translationY = parameters.translationZ = 0;
+                gfxViewer.selectedMesh.position.copy( gfxViewer.selectedMesh.centroid );
+                gfxViewer.selectedMesh.position.sub( gfxViewer.selectedMesh.parent.mergedCentroid );
+            }
+        }, 'resetTranslation' ).name( "reset translation" );
+
+        // When an object is moved with the TransformControls (see obj_viewer.js),
+        // the GUI sliders need to be visually updated, so we do that here.
+        document.addEventListener( 'mousedown', updateTranslationGui );
+
+        function updateTranslationGui() {
+            requestAnimationFrame( updateTranslationGui );
+            if ( gfxViewer.selectedMesh != null ) {
+                parameters.translationX = gfxViewer.selectedMesh.position.x;
+                parameters.translationY = gfxViewer.selectedMesh.position.y;
+                parameters.translationZ = gfxViewer.selectedMesh.position.z;
+            }
+            for ( var i in translationFolder.__controllers )
+                translationFolder.__controllers[i].updateDisplay();
+        }
     }
 
 }
